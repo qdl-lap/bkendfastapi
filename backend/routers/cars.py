e2c1673c625161cac8c3d2279a23b959f111029c
qdl-lap <la_phu@yahoo.com>
2025-07-21 13:08:07 +0100

    Correcting settings key name.

--- backend/routers/cars.py
+++ backend/routers/cars.py
@@ -28,7 +28,7 @@ CARS_PER_PAGE = 10  # Number of cars per page
 cloudinary.config(
     cloud_name=settings.CLOUDINARY_CLOUD_NAME,
     api_key=settings.CLOUDINARY_API_KEY,
-    api_secret=settings.CLOUDINARY_API_SECRET
+    api_secret=settings.CLOUDINARY_SECRET_KEY
 )
 
 @router.post(
